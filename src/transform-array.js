28b4cd0cabc9d8405c5e5fa608c585c7a1f1892c
Maksim Aisin <maksim.aisin@mail.ru>
2022-05-01 22:39:01 +0300

    transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,23 +1,49 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require('../extensions/index.js')
 
 /**
  * Create transformed array based on the control sequences that original
  * array contains
- * 
+ *
  * @param {Array} arr initial array
  * @returns {Array} transformed array
- * 
+ *
  * @example
- * 
+ *
  * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- * 
+ *
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+    if (!Array.isArray(arr))
+        throw new Error(`'arr' parameter must be an instance of the Array!`)
+
+    let current = arr.slice()
+
+    for (let i = 0; i < current.length; i++) {
+        if (current[i] === '--discard-next') {
+            current[i] = 'remove'
+            current[i + 1] = 'remove'
+        }
+
+        if (current[i] === '--discard-prev') {
+            current[i] = 'remove'
+            current[i - 1] = 'remove'
+        }
+
+        if (current[i] === '--double-next') {
+            current[i] = current[i + 1]
+        }
+
+        if (current[i] === '--double-prev') {
+            current[i] = current[i - 1]
+        }
+    }
+
+    let result = current.filter((elem) => elem !== 'remove')
+
+    return result
 }
 
 module.exports = {
-  transform
-};
+    transform,
+}
--- src/what-season.js
+++ src/what-season.js
@@ -26,33 +26,21 @@ function getSeason(date) {
         case 1:
         case 11:
             return 'winter'
-            break
         case 2:
         case 3:
         case 4:
             return 'spring'
-            break
         case 5:
         case 6:
         case 7:
             return 'summer'
-            break
         case 8:
         case 9:
         case 10:
             return 'autumn'
-            break
         default:
             return 'Unable to determine the time of year!'
     }
-    // if (
-    // isNaN(date.getMonth()) ||
-    //     !(Object.prototype.toString.call(date) === '[Object Date]')
-    // )
-    //     throw new Error('Invalid date!')
-
-    // if (!(Object.prototype.toString.call(date) === '[Object Date]'))
-    //     throw new Error('Invalid date!')
 }
 
 module.exports = {
