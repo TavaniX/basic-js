b8b1d54cf110498202973642e479ff72691a27b4
Maksim Aisin <maksim.aisin@mail.ru>
2022-05-01 18:55:12 +0300

    what-season

--- src/what-season.js
+++ src/what-season.js
@@ -11,33 +11,44 @@ const { NotImplementedError } = require('../extensions/index.js')
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  *
  */
-function getSeason(/* date */) {
-    try {
-        switch (date.getMonth()) {
-            case 0:
-            case 1:
-            case 11:
-                return 'winter'
-                break
-            case 2:
-            case 3:
-            case 4:
-                return 'spring'
-                break
-            case 5:
-            case 6:
-            case 7:
-                return 'summer'
-                break
-            case 8:
-            case 9:
-            case 10:
-                return 'autumn'
-                break
-            default:
-                throw new Error('Invalid date!')
+
+function getSeason(date) {
+    if (date instanceof Date) {
+        if (isNaN(date.getMonth()))
+            return 'Unable to determine the time of the year'
+
+        if (!(Object.prototype.toString.call(date) === '[Object Date]'))
+            return 'Unable to determine the time of the year'
+
+        try {
+            switch (date.getMonth()) {
+                case 0:
+                case 1:
+                case 11:
+                    return 'winter'
+                    break
+                case 2:
+                case 3:
+                case 4:
+                    return 'spring'
+                    break
+                case 5:
+                case 6:
+                case 7:
+                    return 'summer'
+                    break
+                case 8:
+                case 9:
+                case 10:
+                    return 'autumn'
+                    break
+                default:
+                    throw new Error('Invalid date!')
+            }
+        } catch (e) {
+            throw new Error('Invalid date!')
         }
-    } catch (e) {
+    } else {
         throw new Error('Invalid date!')
     }
 }
