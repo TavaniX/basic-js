0f029e86796a34f573720d7ad30399ce9bf12a4b
Maksim Aisin <maksim.aisin@mail.ru>
2022-05-01 21:43:20 +0300

    what-season

--- src/what-season.js
+++ src/what-season.js
@@ -15,42 +15,43 @@ const { NotImplementedError } = require('../extensions/index.js')
 function getSeason(date) {
     if (date == null) return 'Unable to determine the time of year!'
 
-    // if (!(Object.prototype.toString.call(date) === '[Object Date]'))
-    //     throw new Error('Invalid date!')
-
     try {
-        switch (date.getMonth()) {
-            case 0:
-            case 1:
-            case 11:
-                return 'winter'
-                break
-            case 2:
-            case 3:
-            case 4:
-                return 'spring'
-                break
-            case 5:
-            case 6:
-            case 7:
-                return 'summer'
-                break
-            case 8:
-            case 9:
-            case 10:
-                return 'autumn'
-                break
-            default:
-                return 'Unable to determine the time of year!'
-        }
+        date.getUTCMonth()
     } catch (e) {
         throw new Error('Invalid date!')
     }
 
+    switch (date.getUTCMonth()) {
+        case 0:
+        case 1:
+        case 11:
+            return 'winter'
+            break
+        case 2:
+        case 3:
+        case 4:
+            return 'spring'
+            break
+        case 5:
+        case 6:
+        case 7:
+            return 'summer'
+            break
+        case 8:
+        case 9:
+        case 10:
+            return 'autumn'
+            break
+        default:
+            return 'Unable to determine the time of year!'
+    }
     // if (
     // isNaN(date.getMonth()) ||
     //     !(Object.prototype.toString.call(date) === '[Object Date]')
     // )
+    //     throw new Error('Invalid date!')
+
+    // if (!(Object.prototype.toString.call(date) === '[Object Date]'))
     //     throw new Error('Invalid date!')
 }
 
